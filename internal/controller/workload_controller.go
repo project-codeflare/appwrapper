939319684cc77439c274642f676631c05023e0ba
David Grove <groved@us.ibm.com>
2024-02-12 16:06:06 -0500

    finish implementation of RunWithPodSetsInfo
    1. inject Kueue's PodSetInfo into all PodTemplateSpecs
    2. inject AppWrapper label into all PodTemplateSpecs
    3. simplify samples by removing label

--- internal/controller/workload_controller.go
+++ internal/controller/workload_controller.go
@@ -17,15 +17,19 @@ limitations under the License.
 package controller
 
 import (
+	"encoding/json"
 	"fmt"
+	"strings"
 
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	kueue "sigs.k8s.io/kueue/apis/kueue/v1beta1"
 	"sigs.k8s.io/kueue/pkg/controller/jobframework"
 	"sigs.k8s.io/kueue/pkg/podset"
+	utilmaps "sigs.k8s.io/kueue/pkg/util/maps"
 
 	workloadv1beta2 "github.com/project-codeflare/appwrapper/api/v1beta2"
 )
@@ -90,7 +94,109 @@ func (aw *AppWrapper) PodSets() []kueue.PodSet {
 
 func (aw *AppWrapper) RunWithPodSetsInfo(podSetsInfo []podset.PodSetInfo) error {
 	aw.Spec.Suspend = false
-	return nil // TODO
+
+	// Update aw.Spec.Components to inject our labels and Kueue's PodSetInfo into every nested PodTemplateSpec
+	podSetsInfoIndex := 0
+	for componentIndex := range aw.Spec.Components {
+		component := &aw.Spec.Components[componentIndex]
+		objChanged := false
+		obj := &unstructured.Unstructured{}
+		if _, _, err := unstructured.UnstructuredJSONScheme.Decode(component.Template.Raw, nil, obj); err != nil {
+			return err
+		}
+
+		for _, podSet := range component.PodSets {
+			podSetsInfoIndex += 1
+			if podSetsInfoIndex <= len(podSetsInfo) {
+				currentInfo := podSetsInfo[podSetsInfoIndex-1]
+				parts := strings.Split(podSet.Path, ".")
+				p := obj.UnstructuredContent()
+				var ok bool
+				for i := 1; i < len(parts); i++ {
+					p, ok = p[parts[i]].(map[string]interface{})
+					if !ok {
+						return fmt.Errorf("path element %v not found (segment %v of %v)", parts[i], i, len(parts))
+					}
+				}
+				objChanged = true // Even if currentInfo is empty, we will still add appWrapperLabel to p.metadata.labels
+
+				if _, ok := p["metadata"]; !ok {
+					p["metadata"] = make(map[string]interface{})
+				}
+				metadata := p["metadata"].(map[string]interface{})
+
+				// Annotations
+				if len(currentInfo.Annotations) > 0 {
+					annotations := metadata["annotations"].(map[string]string)
+					if err := utilmaps.HaveConflict(annotations, currentInfo.Annotations); err != nil {
+						return podset.BadPodSetsUpdateError("annotations", err)
+					}
+					metadata["annotations"] = utilmaps.MergeKeepFirst(annotations, currentInfo.Annotations)
+				}
+
+				// Labels
+				if _, ok := metadata["labels"]; !ok {
+					metadata["labels"] = make(map[string]string)
+				}
+				labels := metadata["labels"].(map[string]string)
+				labels[appWrapperLabel] = aw.Name
+				if len(currentInfo.Labels) > 0 {
+					if err := utilmaps.HaveConflict(labels, currentInfo.Labels); err != nil {
+						return podset.BadPodSetsUpdateError("labels", err)
+					}
+					labels = utilmaps.MergeKeepFirst(labels, currentInfo.Labels)
+				}
+				metadata["labels"] = labels
+
+				spec := p["spec"].(map[string]interface{}) // AppWrapper ValidatingAC ensures this succeeds
+
+				// NodeSelectors
+				if len(currentInfo.NodeSelector) > 0 {
+					if _, ok := spec["nodeSelector"]; !ok {
+						spec["nodeSelector"] = make(map[string]string)
+					}
+					selector := spec["nodeSelector"].(map[string]string)
+					if err := utilmaps.HaveConflict(selector, currentInfo.NodeSelector); err != nil {
+						return podset.BadPodSetsUpdateError("nodeSelector", err)
+					}
+					spec["nodeSelector"] = utilmaps.MergeKeepFirst(selector, currentInfo.NodeSelector)
+				}
+
+				// Tolerations
+				if len(currentInfo.Tolerations) > 0 {
+					if _, ok := spec["tolerations"]; !ok {
+						spec["tolerations"] = []interface{}{}
+					}
+					tolerations := spec["tolerations"].([]interface{})
+					for _, addition := range currentInfo.Tolerations {
+						bytes, err := json.Marshal(addition)
+						if err != nil {
+							return err
+						}
+						tmp := &unstructured.Unstructured{}
+						if _, _, err := unstructured.UnstructuredJSONScheme.Decode(bytes, nil, tmp); err != nil {
+							return err
+						}
+						tolerations = append(tolerations, tmp.UnstructuredContent())
+					}
+				}
+			}
+		}
+
+		if objChanged {
+			bytes, err := obj.MarshalJSON()
+			if err != nil {
+				return err
+			}
+			component.Template.Raw = bytes
+		}
+	}
+
+	if podSetsInfoIndex != len(podSetsInfo) {
+		return podset.BadPodSetsInfoLenError(podSetsInfoIndex, len(podSetsInfo))
+	}
+
+	return nil
 }
 
 func (aw *AppWrapper) RestorePodSetsInfo(podSetsInfo []podset.PodSetInfo) bool {
