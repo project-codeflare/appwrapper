de7fbf883c27daa93bc209b486d1960033355a8b
David Grove <dgrove-oss@users.noreply.github.com>
2024-03-05 13:48:20 -0500

    factor out controller initialization logic (#41)

--- internal/controller/appwrapper_config.go
+++ internal/controller/appwrapper_config.go
@@ -16,7 +16,49 @@ limitations under the License.
 
 package controller
 
+import (
+	"context"
+	"fmt"
+	"os"
+
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/kueue/pkg/controller/jobframework"
+)
+
 type AppWrapperConfig struct {
 	ManageJobsWithoutQueueName bool   `json:"manageJobsWithoutQueueName,omitempty"`
 	ServiceAccountName         string `json:"serviceAccountName,omitempty"`
+}
+
+// SetupWithManager creates and configures all components of the AppWrapper controller
+func SetupWithManager(ctx context.Context, mgr ctrl.Manager, config *AppWrapperConfig) error {
+	if err := WorkloadReconciler(
+		mgr.GetClient(),
+		mgr.GetEventRecorderFor("kueue"),
+		jobframework.WithManageJobsWithoutQueueName(config.ManageJobsWithoutQueueName),
+	).SetupWithManager(mgr); err != nil {
+		return fmt.Errorf("workload controller: %w", err)
+	}
+
+	if err := (&AppWrapperReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+		Config: config,
+	}).SetupWithManager(mgr); err != nil {
+		return fmt.Errorf("appwrapper controller: %w", err)
+	}
+
+	if os.Getenv("ENABLE_WEBHOOKS") != "false" {
+		if err := (&AppWrapperWebhook{
+			Config: config,
+		}).SetupWebhookWithManager(mgr); err != nil {
+			return fmt.Errorf("appwrapper webhook: %w", err)
+		}
+	}
+
+	if err := jobframework.SetupWorkloadOwnerIndex(ctx, mgr.GetFieldIndexer(), GVK); err != nil {
+		return fmt.Errorf("appwrapper indexer: %w", err)
+	}
+
+	return nil
 }
