4b3eb3a9d60b20f4e98c3d858d36e614fe6cb4fd
David Grove <dgrove-oss@users.noreply.github.com>
2024-03-15 10:18:22 -0400

    Update to Kueue 0.6.1 (#55)

--- hack/deploy-kueue.sh
+++ hack/deploy-kueue.sh
@@ -14,16 +14,12 @@
 
 # Installs a kueue release onto an existing cluster
 
-KUEUE_VERSION=v0.6.0
+KUEUE_VERSION=v0.6.1
 
 export ROOT_DIR="$(dirname "$(dirname "$(readlink -fn "$0")")")"
 
-echo "Downloading and patching Kueue ${KUEUE_VERSION} manifests"
-wget https://github.com/kubernetes-sigs/kueue/releases/download/${KUEUE_VERSION}/manifests.yaml -O $ROOT_DIR/hack/kueue/kueue-manifest.yaml
-patch -p 0 $ROOT_DIR/hack/kueue/kueue-manifest.yaml <  $ROOT_DIR/hack/kueue/patch-01-v0.6.0.txt
-
 echo "Deploying Kueue version $KUEUE_VERSION"
-kubectl apply --server-side -f $ROOT_DIR/hack/kueue/kueue-manifest.yaml
+kubectl apply --server-side -f https://github.com/kubernetes-sigs/kueue/releases/download/${KUEUE_VERSION}/manifests.yaml
 
 # Sleep until the kueue manager is running
 echo "Waiting for pods in the kueue-system namespace to become ready"
@@ -33,15 +29,13 @@ do
 done
 echo ""
 
-rm -f $ROOT_DIR/hack/kueue/kueue-manifest.yaml
-
 # Define a default local queue in the default namespace
 echo "Attempting to define default local queue"
 
 # This won't work until kueue's webhooks are actually configured and working,
 # so first sleep for five seconds, then try it in a loop
 sleep 5
-until kubectl apply -f $ROOT_DIR/hack/kueue/default-queues.yaml
+until kubectl apply -f $ROOT_DIR/hack/default-queues.yaml
 do
     echo -n "." && sleep 1;
 done
